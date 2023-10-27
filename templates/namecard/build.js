09a8265ab4c1aea68671118e727092b850786685
depapp <depa.article@gmail.com>
2023-10-27 18:15:56 +0700

    some improvements

--- templates/namecard/build.js
+++ templates/namecard/build.js
@@ -1,20 +1,18 @@
 'use strict'
 
-// Pull in our modules
 const chalk = require('chalk')
 const boxen = require('boxen')
 const fs = require('fs')
 const path = require('path')
 const namecardData = require('./namecardData.js')
 
-// Define options for Boxen
 const options = {
   padding: 2,
   margin: 2,
-  borderStyle: 'round'
+  borderStyle: 'round',
+  textAlignment: 'center'
 }
 
-// Text + chalk definitions
 const data = {
   name: chalk.green.bold(namecardData.fullname),
   handle: chalk.white.bold(namecardData.website),
@@ -22,31 +20,31 @@ const data = {
   github: chalk.gray('github.com/') + chalk.white(namecardData.githubusername),
   linkedin: chalk.gray('linkedin.com/in/') + chalk.white(namecardData.linkedin),
   twitter: chalk.gray('twitter.com/') + chalk.white(namecardData.twitter),
-  card: chalk.red.bold('npx ') + chalk.green.bold(namecardData.githubusername),
-  labelWork: chalk.white.bold('     Work 👔 '),
-  labelGitHub: chalk.white.bold('   GitHub 🐙 '),
-  labelLinkedIn: chalk.white.bold(' LinkedIn 🔗 '),
-  labelTwitter: chalk.white.bold('  Twitter 🐦 '),
-  labelCard: chalk.white.bold('     Card 📇 ')
+  card: chalk.gray('npx ') + chalk.green.bold(namecardData.githubusername),
+  labelWork: chalk.white.bold('Work 👔 '),
+  labelGitHub: chalk.white.bold('GitHub 🐙 '),
+  labelLinkedIn: chalk.white.bold('LinkedIn 🔗 '),
+  labelTwitter: chalk.white.bold('Twitter 🐦 '),
+  labelCard: chalk.white.bold('Card 📇 ')
 }
 
-// Actual strings we're going to output
 const newline = '\n'
-const heading = `${data.name} ${data.handle}`
+const heading = `${data.name}`
+const headingwebsite = `${data.handle}`
 const working = `${data.labelWork}  ${data.work}`
 const githubing = `${data.labelGitHub}  ${data.github}`
 const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
 const twittering = `${data.labelTwitter}  ${data.twitter}`
 const carding = `${data.labelCard}  ${data.card}`
 
-// Put all our output together into a single variable so we can use boxen effectively
-const output = heading + // data.name + data.handle
-               newline + newline + newline + // Add one whole blank line
-               working + newline + newline + // data.labelWork + data.work
-               githubing + newline + // data.labelGitHub + data.github
-               linkedining + newline + // data.labelLinkedIn + data.linkedin
+const output = heading + newline +
+               headingwebsite +
+               newline + newline + newline +
+               working + newline + newline +
+               githubing + newline +
+               linkedining + newline +
                twittering +
-               newline + newline + newline + // Add one whole blank line
+               newline + newline + newline +
                carding
 
-fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)))
+fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.gray.bold(boxen(output, options)))
